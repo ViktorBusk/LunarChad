64ed33b572fd1d6169476ce6c4e9c6994ada3305
ViktorBusk <bviktor228@gmail.com>
2021-12-07 23:19:10 +0100

    Fixed cmp config

--- lua/custom/builtins/cmp.lua
+++ lua/custom/builtins/cmp.lua
@@ -1,9 +1,9 @@
 -- CMP
 lvim.builtin.cmp.formatting.kind_icons = {
-  Text = "",
+  Text = "",
   Method = "",
-  Function = "",
-  Constructor = "",
+  Function = "",
+  Constructor = "",
   Field = "ﰠ",
   Variable = "",
   Class = "ﴯ",
@@ -12,17 +12,30 @@ lvim.builtin.cmp.formatting.kind_icons = {
   Property = "ﰠ",
   Unit = "塞",
   Value = "",
-  Enum = "練",
-  EnumMember = " ",
-  Keyword = "",
+  Enum = "",
+  Keyword = "",
   Snippet = "",
   Color = "",
   File = "",
   Reference = "",
   Folder = "",
+  EnumMember = "",
   Constant = "",
   Struct = "פּ",
   Event = "",
   Operator = "",
   TypeParameter = "",
 }
+
+lvim.builtin.cmp.formatting.source_names = {
+  nvim_lsp = "[LSP]",
+  emoji = "[EMOJI]",
+  path = "[PATH]",
+  calc = "[CALC]",
+  cmp_tabnine = "[TABNINE]",
+  vsnip = "[SNIPP]",
+  luasnip = "[SNIPP]",
+  buffer = "[BUF]",
+}
+
+lvim.builtin.cmp.documentation = {}
