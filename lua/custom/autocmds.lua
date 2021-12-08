4c2a33fccc00faac2849ab6b87358c6c659103bd
ViktorBusk <bviktor228@gmail.com>
2021-12-08 23:18:53 +0100

    Claer doxygen syntax highlights with autocmd

--- lua/custom/autocmds.lua
+++ lua/custom/autocmds.lua
@@ -1,4 +1,5 @@
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 lvim.autocommands.custom_groups = {
   { "BufEnter,BufWinEnter,FileType,WinEnter", "*", "lua require(\"custom.utils\").hide_statusline()" },
+  { "BufEnter", "*", "syntax clear" }, -- Clear doxygen syntax highlights
 }
