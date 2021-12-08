3927ea44b5b540fd0b3c01c0df2b386e9b103099
ViktorBusk <bviktor228@gmail.com>
2021-12-09 00:05:06 +0100

    Only clear syntax for filetypes with format name.extenson

--- lua/custom/autocmds.lua
+++ lua/custom/autocmds.lua
@@ -1,5 +1,5 @@
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 lvim.autocommands.custom_groups = {
   { "BufEnter,BufWinEnter,FileType,WinEnter", "*", "lua require(\"custom.utils\").hide_statusline()" },
-  { "BufEnter", "*", "syntax clear" }, -- Clear doxygen syntax highlights
+  { "BufEnter", "*.*", "syntax clear" }, -- Clear doxygen syntax highlights
 }
