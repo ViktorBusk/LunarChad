68bf331fafa1adff1dd947cda46188b1d095fc34
ViktorBusk <bviktor228@gmail.com>
2022-01-21 12:03:04 +0100

    Updated settings for lvim-gui

--- lua/custom/neovide.lua
+++ lua/custom/neovide.lua
@@ -1,16 +1,13 @@
 -- https://github.com/LunarVim/LunarVim/issues/1553#issuecomment-920247252
 
 -- Neovide
-vim.opt.guifont = "monospace:h12"
+-- vim.opt.guifont = "JetBrainsMono Nerd Font:h13"
 vim.g.neovide_refresh_rate=60
 vim.g.neovide_cursor_animation_length=0.035
-
--- vim.g.neovide_window_floating_opacity = 1
+vim.g.neovide_transparency=1.0
+vim.g.neovide_floating_opacity=1.0
+vim.g.neovide_floating_blur="v:false"
+-- vim.g.neovide_fullscreen=true
 -- vim.g.neovide_cursor_trail_length=0.0
 -- vim.g.neovide_cursor_antialiasing="v:true"
--- vim.g.neovide_transparency=0.0
--- vim.g.neovide_window_floating_opacity = 0
--- vim.g.neovide_floating_blur = 0
--- vim.g.neovide_window_floating_blur = 0
--- vim.g.neovide_fullscreen=v:true
 -- vim.g.neovide_cursor_vfx_mode = "pixiedust"
